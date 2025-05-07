0257c862f5fa12892535e65737d691d31a0509a0
Jürgen Hermes <hermesj@uni-koeln.de>
2025-05-07 16:24:37 +0200

    Status Ende des Seminars

--- src/idh/java/generics/GenericDemo.java
+++ src/idh/java/generics/GenericDemo.java
@@ -3,14 +3,25 @@ package idh.java.generics;
 public class GenericDemo {
 
 	public static void main(String[] args) {
-		StringList sl = new StringList();
+		// Eine Liste für Strings
+		GenericArrayList<String> sl = new GenericArrayList<String>();
 		sl.add("Erster");
 		sl.add("Zweiter");
 		sl.add("Dritter");
 		
+		// Eine Liste für Students
 		Student studi1 = new Student("Max Moritz");
-
-		//sl.add(studi1);
+		GenericArrayList<Student> sl2 = new GenericArrayList<Student>();
+		sl2.add(studi1);
+		System.out.println(sl2.get(0));
+		
+		// Eine Liste für Integers
+		GenericArrayList<Integer> gal1 = new GenericArrayList<Integer>(10);
+		gal1.add(1002);
+		gal1.remove(0);
+		
+		// Alles mit der gleichen Klasse realisiert!
+		
 
 	}
 
